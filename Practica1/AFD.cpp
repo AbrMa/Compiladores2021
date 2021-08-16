25390aede64a63c7338f1fb17ca5b9c2182364ee
Abrma <abrma98@gmail.com>
2021-08-16 17:33:48 -0500

    Clase AFD

--- Practica1/AFD.cpp
+++ Practica1/AFD.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class AFD {
+	public:
+		vector <char> sigma; // simbolos de entrada
+		vector <int> Q; // conjunto de estados
+		int q0; // estado inicial
+		vector <int> F; // conjunto estados finales
+		unordered_map <int, char> f // función de transición
+};
 
 int main () {
 	return 0;
