5a8ce172a2343a3586ae565a004ac6260a3b256d
Abrma <abrma98@gmail.com>
2021-08-16 17:53:58 -0500

    Clase FD y FN

--- Practica1/Automata.cpp
+++ Practica1/Automata.cpp
@@ -4,13 +4,22 @@
 
 using namespace std;
 
-class AFD {
+class Automata {
 	public:
 		vector <char> sigma; // simbolos de entrada
 		vector <int> Q; // conjunto de estados
 		int q0; // estado inicial
 		vector <int> F; // conjunto estados finales
-		unordered_map <int, char> f // función de transición
+};
+
+class FinDet { 
+	public:
+		unordered_map <int, char> f; // función de transición
+};
+
+class NoDet { 
+	public:
+		unordered_map <int, vector <int> > f; // función de transición
 };
 
 int main () {
