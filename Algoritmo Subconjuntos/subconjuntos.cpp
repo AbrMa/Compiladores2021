8fe3804e3d0d5fd072474daf1c25d16434a0b1ab
AbrMa <abrma98@gmail.com>
2021-10-15 14:27:34 -0500

    Creación de clase AFD con constructores y métodos

--- Algoritmo Subconjuntos/subconjuntos.cpp
+++ Algoritmo Subconjuntos/subconjuntos.cpp
@@ -21,28 +21,76 @@ public:
         this -> simbolo = simbolo;
         this -> estado = estado;
     }    
+
     Transicion(int estado, int simbolo) {
         this -> simbolo = simbolo;
         this -> estado = estado;
     }    
+
+    //Métodos
+    char get_simbolo(void) {
+        return simbolo;
+    }
+
+    int get_estado(void) {
+        return estado;
+    }
+};
+
+class AFN {
+    set<int> estados;
+    set<char> alfabeto;
+    vector<vector<Transicion>> transcisiones;
+    int estado_inicial;
+    set<int> estados_aceptacion; 
+
+public:
+    //constructor
+    AFN(set<int> estados, set<char> alfabeto, int estado_inicial, set<int> estados_aceptacion) {
+        this -> estados = estados;
+        this -> alfabeto = alfabeto;
+        this -> estado_inicial = estado_inicial;
+        this -> estados_aceptacion = estados_aceptacion;
+        transcisiones.resize(estados.size());
+    }
+
+    //Métodos    
+    void inserta_transicion(int origen, char simbolo, int destino) {
+        //origen ---simbolo---> destino
+        transcisiones[origen].push_back(Transicion(simbolo,destino));
+    }
+
+    void imprime_transiciones(void) {
+        for (int i = 0; i < transcisiones.size(); i++) {
+            cout << "Estando en " << i << " puedo ir a :" << endl;
+            for (int j = 0; j < transcisiones[i].size(); j++) {
+                cout << "\t" << transcisiones[i][j].get_estado() << " con '";
+                cout << transcisiones[i][j].get_simbolo() << "'" << endl;
+            } 
+        }
+    }
 };
 
 int main() {
     //AFN 'N' para (a|b)*abb
-    vector<vector<Transicion>> N(11);
-    N[0].push_back(Transicion('E', 1)); 
-    N[0].push_back(Transicion('E', 7)); 
-    N[1].push_back(Transicion('E', 2)); 
-    N[1].push_back(Transicion('E', 4)); 
-    N[2].push_back(Transicion('a', 3)); 
-    N[3].push_back(Transicion('E', 6)); 
-    N[4].push_back(Transicion('b', 5)); 
-    N[5].push_back(Transicion('E', 6)); 
-    N[6].push_back(Transicion('E', 7)); 
-    N[6].push_back(Transicion('E', 1)); 
-    N[7].push_back(Transicion('a', 8)); 
-    N[8].push_back(Transicion('b', 9)); 
-    N[9].push_back(Transicion('b', 10)); 
-
+    set<int> estados = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ,10};
+    set<char> alfabeto = {'a', 'b'};
+    set<int> estados_aceptacion = {10};
+    AFN N = AFN(estados, alfabeto, 0, estados_aceptacion);
+    N.inserta_transicion(0, 'E', 1);
+    N.inserta_transicion(0, 'E', 7);
+    N.inserta_transicion(1, 'E', 2);
+    N.inserta_transicion(1, 'E', 4);
+    N.inserta_transicion(2, 'a', 3);
+    N.inserta_transicion(3, 'E', 6);
+    N.inserta_transicion(4, 'b', 5);
+    N.inserta_transicion(4, 'b', 5);
+    N.inserta_transicion(5, 'E', 6);
+    N.inserta_transicion(6, 'E', 1);
+    N.inserta_transicion(6, 'E', 7);
+    N.inserta_transicion(7, 'a', 8);
+    N.inserta_transicion(8, 'b', 9);
+    N.inserta_transicion(9, 'b', 10);
+    N.imprime_transiciones();
     return 0;
 }
