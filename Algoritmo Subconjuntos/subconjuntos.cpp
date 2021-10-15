0dafc603b3a713561a7901ac9b585013a6010600
AbrMa <abrma98@gmail.com>
2021-10-15 13:10:32 -0500

    Creación del automata N y creación de nuevo constructor

--- Algoritmo Subconjuntos/subconjuntos.cpp
+++ Algoritmo Subconjuntos/subconjuntos.cpp
@@ -16,7 +16,12 @@ class Transicion {
     int estado;
 
 public:
+    //Constructores
     Transicion(char simbolo, int estado) {
+        this -> simbolo = simbolo;
+        this -> estado = estado;
+    }    
+    Transicion(int estado, int simbolo) {
         this -> simbolo = simbolo;
         this -> estado = estado;
     }    
@@ -24,8 +29,20 @@ public:
 
 int main() {
     //AFN 'N' para (a|b)*abb
-
     vector<vector<Transicion>> N(11);
     N[0].push_back(Transicion('E', 1)); 
+    N[0].push_back(Transicion('E', 7)); 
+    N[1].push_back(Transicion('E', 2)); 
+    N[1].push_back(Transicion('E', 4)); 
+    N[2].push_back(Transicion('a', 3)); 
+    N[3].push_back(Transicion('E', 6)); 
+    N[4].push_back(Transicion('b', 5)); 
+    N[5].push_back(Transicion('E', 6)); 
+    N[6].push_back(Transicion('E', 7)); 
+    N[6].push_back(Transicion('E', 1)); 
+    N[7].push_back(Transicion('a', 8)); 
+    N[8].push_back(Transicion('b', 9)); 
+    N[9].push_back(Transicion('b', 10)); 
+
     return 0;
 }
