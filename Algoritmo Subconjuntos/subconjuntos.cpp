e43ad6ffd18ef1627687b20799c701ee442388a5
AbrMa <abrma98@gmail.com>
2021-10-15 12:39:38 -0500

    Descripción básica del programa

---
+++ Algoritmo Subconjuntos/subconjuntos.cpp
@@ -0,0 +1,16 @@
+/*
+    Descripción : 
+        La construcción de subconjuntos de un AFD, a partir de un AFN.
+
+    Entrada:
+        Un AFN 'N'.
+
+    Salida:
+        Un AFD 'D' que acepta el mismo lenguaje que N.    
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    return 0;
+}
